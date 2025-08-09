21e19faa27931408b13c5a0ed4a4667aba88a483
madnate <ca.nate104@gmail.com>
2025-08-09 23:30:54 +0000

    Add setup tailwaind

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         sans: ['Inter', 'system-ui', 'sans-serif'],
       },
       animation: {
-        'fade-in': 'fadeIn 0.5s ease-in-out',
+        'fade-in': 'fade-in 0.5s ease-in-out',
         'slide-up': 'slideUp 0.3s ease-out',
         'bounce-subtle': 'bounceSubtle 0.6s ease-in-out',
       },
